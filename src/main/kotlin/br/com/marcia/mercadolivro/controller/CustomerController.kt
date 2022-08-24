dddcbfdd49efd99bc72dd395731610057cc388d3
Marcia Castagna <marciafc.info@gmail.com>
2022-08-23 22:46:36 -0300

    Include endpoints

--- src/main/kotlin/br/com/marcia/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/br/com/marcia/mercadolivro/controller/CustomerController.kt
@@ -1,6 +1,7 @@
 package br.com.marcia.mercadolivro.controller
 
 import br.com.marcia.mercadolivro.controller.request.PostCustomerRequest
+import br.com.marcia.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -11,14 +12,23 @@ class CustomerController {
 
     val customers = mutableListOf<CustomerModel>()
 
+    // @RequestParam name: String?  -> opcional (tem ponto de interrogação)
     @GetMapping
-    fun getCustomer(): List<CustomerModel> {
+    fun getAll(@RequestParam name: String?): List<CustomerModel> {
+
+        // name?. -> só fará essa pesquisa se name for diferente de null
+        name?.let {
+            return customers.filter { it.name.contains(name, true) }
+        }
+
         return customers
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest) {
+
+        // if já retorna
         val id = if(customers.isEmpty()) {
             1
         } else {
@@ -27,5 +37,25 @@ class CustomerController {
         }.toString()
 
         customers.add(CustomerModel(id, customer.name, customer.email))
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customers.filter { it.id == id }.first()
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
+        customers.filter { it.id == id }.first().let {
+            it.name = customer.name
+            it.email = customer.email
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: String) {
+        customers.removeIf { it.id == id }
     }
 }
---
+++ src/main/kotlin/br/com/marcia/mercadolivro/controller/request/PutCustomerRequest.kt
@@ -0,0 +1,7 @@
+package br.com.marcia.mercadolivro.controller.request
+
+data class PutCustomerRequest (
+    var name: String,
+
+    var email: String
+)
