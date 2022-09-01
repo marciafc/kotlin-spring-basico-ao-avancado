2539563de10ee74bd409e992491a1fe36125b471
Marcia Castagna <marciafc.info@gmail.com>
2022-09-01 05:22:57 -0300

    Include integration tests

--- src/test/kotlin/br/com/marcia/mercadolivro/controller/CustomerControllerTest.kt
+++ src/test/kotlin/br/com/marcia/mercadolivro/controller/CustomerControllerTest.kt
@@ -1,8 +1,10 @@
 package br.com.marcia.mercadolivro.controller
 
+import br.com.marcia.mercadolivro.controller.request.PostCustomerRequest
 import com.fasterxml.jackson.databind.ObjectMapper
 import br.com.marcia.mercadolivro.helper.buildCustomer
 import br.com.marcia.mercadolivro.repository.CustomerRepository
+import br.com.marcia.mercadolivro.security.UserCustomDetails
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
@@ -10,18 +12,21 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import kotlin.random.Random
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ContextConfiguration
 @ActiveProfiles("test")
-@WithMockUser
+@WithMockUser // mock usuário -> parte de autenticação
 class CustomerControllerTest {
 
     @Autowired
@@ -40,13 +45,14 @@ class CustomerControllerTest {
     fun tearDown() = customerRepository.deleteAll()
 
     @Test
-    fun `should return all customers`() {
+    fun `should return all customers when get all`() {
 
         val customer1 = customerRepository.save(buildCustomer())
         val customer2 = customerRepository.save(buildCustomer())
 
         mockMvc.perform(get("/customers"))
             .andExpect(status().isOk)
+            .andExpect(jsonPath("$.length()").value(2)) // retornou DOIS itens na lista
             .andExpect(jsonPath("$[0].id").value(customer1.id))
             .andExpect(jsonPath("$[0].name").value(customer1.name))
             .andExpect(jsonPath("$[0].email").value(customer1.email))
@@ -56,5 +62,65 @@ class CustomerControllerTest {
             .andExpect(jsonPath("$[1].email").value(customer2.email))
             .andExpect(jsonPath("$[1].status").value(customer2.status.name))
     }
+
+    @Test
+    fun `should filter all customers by name when get all`() {
+
+        val customer1 = customerRepository.save(buildCustomer(name = "Gustavo"))
+        customerRepository.save(buildCustomer(name = "Daniel"))
+
+        mockMvc.perform(get("/customers?name=Gus"))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$.length()").value(1)) // só retornou UM item na lista
+                .andExpect(jsonPath("$[0].id").value(customer1.id))
+                .andExpect(jsonPath("$[0].name").value(customer1.name))
+                .andExpect(jsonPath("$[0].email").value(customer1.email))
+                .andExpect(jsonPath("$[0].status").value(customer1.status.name))
+    }
+
+    @Test
+    fun `should create customer`() {
+
+        val request = PostCustomerRequest("fake name", "${Random.nextInt()}@fakeemail.com", "123456")
+
+        mockMvc.perform(post("/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated)
+
+        // Verificar se salvou
+        val customers = customerRepository.findAll().toList()
+        assertEquals(1, customers.size)
+        assertEquals(request.name, customers[0].name)
+        assertEquals(request.email, customers[0].email)
+    }
+
+    @Test
+    fun `should get user by id when user has the same id`() {
+
+        val customer = customerRepository.save(buildCustomer())
+
+        // with(user(UserCustomDetails(customer))) -> dados do usuário com autenticação
+        mockMvc.perform(get("/customers/${customer.id}").with(user(UserCustomDetails(customer))))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$.id").value(customer.id))
+                .andExpect(jsonPath("$.name").value(customer.name))
+                .andExpect(jsonPath("$.email").value(customer.email))
+                .andExpect(jsonPath("$.status").value(customer.status.name))
+    }
+
+    @Test
+    fun `should return forbidden when user has diffent id`() {
+
+        val customer = customerRepository.save(buildCustomer())
+
+        // Tentando acessar um id de customer que o usuário não pode acessar
+        mockMvc.perform(get("/customers/0").with(user(UserCustomDetails(customer))))
+                .andExpect(status().isForbidden)
+                .andExpect(jsonPath("$.httpCode").value(403))
+                .andExpect(jsonPath("$.message").value("Access Denied"))
+                .andExpect(jsonPath("$.internalCode").value("ML-000"))
+    }
+
 
 }
